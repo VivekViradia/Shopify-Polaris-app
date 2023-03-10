ab72074e0ee116da89628dbffbf376d723024f09
Vivek Viradia <vivekviradia123@gmail.com>
2023-03-10 15:38:31 +0530

    API fetched using useAppQuery

--- web/frontend/components/SectionStepOne.jsx
+++ web/frontend/components/SectionStepOne.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from "react";
 import {
   Card,
-  Page,
   Layout,
   Stack,
   Select,
@@ -11,16 +10,16 @@ import {
   useIndexResourceState,
 } from "@shopify/polaris";
 import { useState, useCallback } from "react";
-import { useAuthenticatedFetch } from "../hooks/useAuthenticatedFetch";
-    
+// import { useAuthenticatedFetch } from "../hooks/useAuthenticatedFetch";
+import { useAppQuery , useAuthenticatedFetch} from "../hooks";
 
 const SectionStepOne = () => {
   const fetch = useAuthenticatedFetch();
 
   const [selected, setSelected] = useState("today");
   const [selected2, setSelected2] = useState("is");
-  const [selected3, setSelected3] = useState('');
-  const [isLoading, setIsLoading] = useState(false);
+  const [selected3, setSelected3] = useState("");
+  const [loading, setIsLoading] = useState(false);
 
   const handleSelectChange = useCallback((value) => setSelected(value), []);
   const handleSelectChange2 = useCallback((value) => setSelected2(value), []);
@@ -36,16 +35,47 @@ const SectionStepOne = () => {
     { label: "is not", value: "is not" },
   ];
 
-  const fetchData = async () => {
-    try {
-      const response = await fetch("/api/products")
-      console.log("Data",await response.json())
-    } catch(err) {
-      console.log('Error',err)
-  }
-}
+  // const { data, isLoading: isLoadingTrue } = useAppQuery({
+  //   url: "/api/products",
+  //   reactQueryOptions: {
+  //     onSuccess: () => {
+  //       setIsLoading(true);
+  //     },
+  //   },
+  // });
 
-fetchData()
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("/api/products");
+        console.log("Data", await response.json());
+      } catch (err) {
+        console.log("Error", err);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  // const { selectedResources } = useIndexResourceState(data);
+  // console.log("DATA", data);
+
+  // const rowMark = () => {
+  //   (data && data.length > 0) ? data.map(({ ids, title, handle, vendor }, index) => {
+  //     return <IndexTable.Row
+  //       key={ids}
+  //       id={ids}
+  //       position={index}
+  //       selected={selectedResources.includes(ids)}
+  //     >
+  //       <IndexTable.Cell>{title}</IndexTable.Cell>
+  //       <IndexTable.Cell>{handle}</IndexTable.Cell>
+  //       <IndexTable.Cell>{vendor}</IndexTable.Cell>
+  //     </IndexTable.Row>
+  //   })
+  //     : <>Hello there</>
+  // }
+
   return (
     <Card>
       <Layout>
@@ -88,7 +118,19 @@ fetchData()
           </ButtonGroup>
         </Layout.Section>
         <Layout.Section>
-    
+          {/* <Card>
+            <IndexTable
+              itemCount={data.length}
+              headings={[
+                { title: "Title" },
+                { title: "Handle" },
+                { title: "Vender" },
+              ]}
+            >
+              hey there
+              {rowMark}
+            </IndexTable>
+          </Card> */}
         </Layout.Section>
         <Layout.Section></Layout.Section>
       </Layout>
